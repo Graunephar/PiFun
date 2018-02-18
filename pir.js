4e421ceb24373fdcaf4458be709ffe7cc06f061f
Graunephar <daniel@graungaard.com>
2018-02-19 00:05:31 +0100

    deleted bad comment

--- pir.js
+++ pir.js
@@ -5,15 +5,10 @@ sensor.watch((err, value) => { // #B
   if (err) exit(err)
   console.log(value ? 'there is someone!' : 'not anymore!')
 })
-function exit(err) {
+function exit (err) {
   if (err) console.log('An error occurred: ' + err)
   sensor.unexport()
   console.log('Bye, bye!')
   process.exit()
 }
 process.on('SIGINT', exit)
-// #A Initialize pin 17 in input mode, 'both' means we want to handle
-//
-both rising and falling interrupt edges
-// #B Listen for state changes on pin 17, if a change is detected
-//
