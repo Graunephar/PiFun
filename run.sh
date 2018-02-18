f9cd5dc20ef668085b24d39f1f23ec95da16c432
Graunephar <daniel@graungaard.com>
2018-02-19 00:06:16 +0100

    Updated run script to run in single terminal

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ killall node #Kill every old instance of node
 cd "$(dirname "$0")"
 
 
-npm install & #install dependencies from package.json (pro-tip use auto-install to keep this in sync)
+npm install #install dependencies from package.json (pro-tip use auto-install to keep this in sync)
 
 #Start node js 
 node pir.js
